3a4fc5fa98d65259352ed86565073eed6b6f1f85
Asmit Mishra <asmit6109@gmail.com>
2026-09-24 20:43:48 +0530

    Add files via upload

---
+++ Lab_Expt1/Q1.py
@@ -0,0 +1,12 @@
+"""WAP TO ENTER TWO INTEGER AND DO THE ALL ARITHMATIC OPERATIONS ON THEM"""
+
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Floor Division =", a // b)
+print("Modulus =", a % b)
+print("Exponentiation =", a ** b)
